0fbdbac3d7d32af682265773553e4ce858c2af08
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-05-07 13:04:36 +0100

    minor

--- contracts/consumption-unit/sign-cu-ts-demo/index.ts
+++ contracts/consumption-unit/sign-cu-ts-demo/index.ts
@@ -14,15 +14,17 @@ let raw_json = {
     ]
 }
 
-// Init test keys
+// 0. Init test keys
 let private_key = Buffer.from("4236627b5a03b3f2e601141a883ccdb23aeef15c910a0789e4343aad394cbf6d", 'hex');
 if (!secp256k1.privateKeyVerify(private_key)) {
     console.log("private key is not valid")
 }
 const pubKeyCompressed = secp256k1.publicKeyCreate(private_key, true);  // 33 bytes
-console.log('Public key (compressed, 33 bytes):', Buffer.from(pubKeyCompressed).toString('hex'));
+let publicKeyHex = Buffer.from(pubKeyCompressed).toString('hex')
 
-//  Hash the message
+console.log('Public key (compressed, 33 bytes):', publicKeyHex);
+
+// 1. Hash the message
 const encoder = new TextEncoder();
 const message = encoder.encode(JSON.stringify(raw_json));
 const msgHash = createHash("sha256").update(message).digest()
@@ -30,8 +32,6 @@ console.log("message hash:", msgHash.toString('hex'))
 
 // Sign the message hash
 const {signature, recid} = secp256k1.ecdsaSign(msgHash, private_key);
-
-// Output compact signature (64 bytes: r + s)
 let signatureHex = Buffer.from(signature).toString('hex')
 console.log('Compact signature (64 bytes):', signatureHex);
 
@@ -41,4 +41,17 @@ console.log('Signature valid?', verified);
 
 if (signatureHex != "dbc7c4d857beea592131a673f5970add886f26a1d1b9cd4fbc68aed82a5b8a342d07d7acb26a12acaacc5cad785534a814c38f67ddacdaf0693943b1aa78fa85") {
     console.error("wrong signature")
+}
+
+// Now you have everything you need to conduct the mint transaction:
+let mintTx = {
+    mint: {
+        token_id: "1",
+        owner: "cosmwasm1j2mmggve9m6fpuahtzvwcrj3rud9cqjz9qva39cekgpk9vprae8s4haddx",
+        extension: {
+            entity: raw_json,
+            signature: signatureHex,
+            public_key: publicKeyHex,
+        },
+    }
 }
