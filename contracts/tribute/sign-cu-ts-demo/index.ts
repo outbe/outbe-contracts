a207d8503c8cecc8c6eb914f512a7f2a5b922888
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-05-14 15:09:54 +0100

    fix json

--- contracts/tribute/sign-cu-ts-demo/index.ts
+++ contracts/tribute/sign-cu-ts-demo/index.ts
@@ -5,9 +5,7 @@ import {createHash} from "node:crypto";
 let raw_json = {
     "token_id": "1",
     "owner": "cosmwasm1j2mmggve9m6fpuahtzvwcrj3rud9cqjz9qva39cekgpk9vprae8s4haddx",
-    "consumption_value": "100",
-    "nominal_quantity": "100",
-    "nominal_currency": "usd",
+    "minor_value_settlement": "100",
     "hashes": [
         "872be89dd82bcc6cf949d718f9274a624c927cfc91905f2bbb72fa44c9ea876d"
     ]
@@ -49,7 +47,6 @@ let mintTx = {
         owner: "cosmwasm1j2mmggve9m6fpuahtzvwcrj3rud9cqjz9qva39cekgpk9vprae8s4haddx",
         extension: {
             entity: raw_json,
-            vector: 1,
             signature: signatureHex,
             public_key: publicKeyHex,
         },
