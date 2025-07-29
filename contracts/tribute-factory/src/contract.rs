17dab4ef833196ce4cfa4545009c83296ff254f0
darcksday <darcksday@gmail.com>
2025-07-29 16:42:57 +0300

    fix tribute owner

--- contracts/tribute-factory/src/contract.rs
+++ contracts/tribute-factory/src/contract.rs
@@ -131,6 +131,7 @@ fn execute_offer_insecure(
         return Err(ContractError::CUEmpty {});
     }
 
+    //TODO change it to info.sender
     let tribute_owner = tribute_input.owner.clone();
 
     let timestamp_date = iso_to_ts(&tribute_input.worldwide_day)?;
@@ -141,7 +142,9 @@ fn execute_offer_insecure(
 
     let tribute_id =
         UNUSED_TOKEN_ID.update(deps.storage, |old| Ok::<u64, ContractError>(old + 1))?;
-
+    
+   
+    
     let settlement_amount = normalize_amount(
         tribute.settlement_base_amount,
         tribute.settlement_atto_amount,
