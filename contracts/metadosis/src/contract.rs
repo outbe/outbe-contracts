eb1f591470eef73943a0ff09273709b162bb132d
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-08-01 14:28:29 +0100

    fix: adjust winners calculation logic and prize distribution

--- contracts/metadosis/src/contract.rs
+++ contracts/metadosis/src/contract.rs
@@ -460,24 +460,21 @@ fn do_execute_touch(
     let mut expected_win_amount = touch_info.touch_limit;
 
     if touch_info.gold_ignot_price.atomics() < touch_info.touch_limit {
-        expected_win_amount = touch_info.gold_ignot_price.atomics();
-        expected_winners_count =
-            (touch_info.touch_limit / touch_info.gold_ignot_price.atomics()).u128() as usize + 1;
+        let winners_count = touch_info.touch_limit / touch_info.gold_ignot_price.atomics();
+        expected_winners_count = winners_count.u128() as usize;
+        expected_win_amount = touch_info.touch_limit / winners_count;
     };
 
     println!("expected_winners_count {}", expected_winners_count);
 
-    let mut winners: Vec<(FullTributeData, Uint128)> = vec![];
+    let mut winners: Vec<FullTributeData> = vec![];
     for tribute in allocated_tributes {
         if WINNERS.has(deps.storage, tribute.token_id.clone()) {
             continue;
         }
-        if !winners.is_empty() && winners.len() == expected_winners_count - 1 {
-            expected_win_amount = touch_info.touch_limit % touch_info.gold_ignot_price.atomics();
-        }
 
         WINNERS.save(deps.storage, tribute.token_id.clone(), &())?;
-        winners.push((tribute, expected_win_amount));
+        winners.push(tribute);
         if expected_winners_count == winners.len() {
             break;
         }
@@ -485,7 +482,7 @@ fn do_execute_touch(
     let winners_len = winners.len();
 
     let mut messages: Vec<SubMsg> = vec![];
-    for (tribute, win_amount) in winners {
+    for tribute in winners {
         let nod_id = format!("{}_{}", tribute.token_id, run_today.number_of_runs);
         let nod_mint = WasmMsg::Execute {
             contract_addr: nod_address.to_string(),
@@ -500,7 +497,7 @@ fn do_execute_touch(
                         floor_rate: Uint128::zero(),
                         nominal_price_minor: tribute.data.tribute_price_minor,
                         issuance_price_minor: exchange_rate.price,
-                        gratis_load_minor: win_amount,
+                        gratis_load_minor: expected_win_amount,
                         floor_price_minor: exchange_rate.price,
                         state: nod::types::State::Issued,
                         owner: tribute.owner.to_string(),
