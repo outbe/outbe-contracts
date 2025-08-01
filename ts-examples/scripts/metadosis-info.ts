7a9dc87b22e444c0172ca9d528aaaeab25f36ed2
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-08-01 13:08:31 +0100

    feat: add history retrieval and enhance token count logging

--- ts-examples/scripts/metadosis-info.ts
+++ ts-examples/scripts/metadosis-info.ts
@@ -1,8 +1,7 @@
 import {getContractAddresses, initClient} from "../lib/clientService";
 import {MetadosisQueryClient} from "../clients/metadosis/Metadosis.client";
-import {MetadosisInfoResponse} from "../clients/metadosis/Metadosis.types";
+import {HistoryResponse, MetadosisInfoResponse} from "../clients/metadosis/Metadosis.types";
 import {NodQueryClient} from "../clients/nod/Nod.client";
-import {NumTokensResponse} from "../clients/nod/Nod.types";
 
 async function main() {
     const {walletClient} = await initClient()
@@ -16,10 +15,18 @@ async function main() {
     console.log("info: ")
     console.log(JSON.stringify(info, null, 2))
 
+    let history: HistoryResponse = await metadosisClient.history()
+    console.log("")
+    console.log("history:")
+    console.log(JSON.stringify(history, null, 2))
+
+    console.log("")
+    const tributeClient = new NodQueryClient(walletClient, await getContractAddresses('TRIBUTE_CONTRACT_ADDRESS'))
+    console.log("Number of Tribute tokens: ", await tributeClient.numTokens())
+
     const nodContractAddress = await getContractAddresses('NOD_CONTRACT_ADDRESS')
     const nodClient = new NodQueryClient(walletClient, nodContractAddress)
-    let tokensResp: NumTokensResponse = await nodClient.numTokens()
-    console.log("Number of Nod tokens: ", tokensResp)
+    console.log("Number of Nod tokens: ", await nodClient.numTokens())
 }
 
 
