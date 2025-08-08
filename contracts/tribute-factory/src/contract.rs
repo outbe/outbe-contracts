097ac4f4caa5c2ce50f0b81cee2f400a474b268a
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-08-08 20:51:06 +0100

    chore: add "demo" feature flag for tribute_owner_l1 in OfferInsecure command

--- contracts/tribute-factory/src/contract.rs
+++ contracts/tribute-factory/src/contract.rs
@@ -72,8 +72,17 @@ pub fn execute(
         ExecuteMsg::OfferInsecure {
             tribute_input,
             zk_proof,
+            #[cfg(feature = "demo")]
             tribute_owner_l1,
-        } => execute_offer_insecure(deps, env, info, tribute_input, zk_proof, tribute_owner_l1),
+        } => execute_offer_insecure(
+            deps,
+            env,
+            info,
+            tribute_input,
+            zk_proof,
+            #[cfg(feature = "demo")]
+            tribute_owner_l1,
+        ),
         ExecuteMsg::BurnAll {} => execute_burn_all(deps, env, info),
     }
 }
@@ -132,7 +141,7 @@ fn execute_offer_insecure(
     info: MessageInfo,
     tribute_input: TributeInputPayload,
     _zk_proof: ZkProof,
-    tribute_owner_l1: Option<Addr>,
+    #[cfg(feature = "demo")] tribute_owner_l1: Option<Addr>,
 ) -> Result<Response, ContractError> {
     let config = CONFIG.load(deps.storage)?;
     let tribute_address = config
@@ -145,7 +154,16 @@ fn execute_offer_insecure(
     }
 
     //TODO change it to info.sender
-    let tribute_owner = tribute_owner_l1.unwrap_or(info.sender.clone());
+    let tribute_owner = {
+        #[cfg(feature = "demo")]
+        {
+            tribute_owner_l1.unwrap_or(info.sender.clone())
+        }
+        #[cfg(not(feature = "demo"))]
+        {
+            info.sender.clone()
+        }
+    };
 
     let timestamp_date = iso_to_ts(&tribute_input.worldwide_day)?;
     // let timestamp_date = _env.block.time.seconds();
@@ -351,6 +369,7 @@ mod tests {
                     },
                     verification_key: Default::default(),
                 },
+                #[cfg(feature = "demo")]
                 tribute_owner_l1: None,
             },
             &[],
