6df7731aa6144831e9a57bb281257abb1a879b2d
latexstone <171574715+latexstone@users.noreply.github.com>
2025-08-19 09:33:13 +0100

    feat: Add Base58 encoding (#76)
    * feat: add Base58Binary for base58 serialization/deserialization
    
    * refactor: replace HexBinary with Base58Binary for serialization

--- contracts/tribute-factory/src/types.rs
+++ contracts/tribute-factory/src/types.rs
@@ -1,13 +1,14 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{HexBinary, Uint128, Uint64};
+use cosmwasm_std::{Uint128, Uint64};
 use outbe_utils::date::Iso8601Date;
+use outbe_utils::Base58Binary;
 
 #[cw_serde]
 pub struct TributeInputPayload {
     /// ID of the draft tribute
-    pub tribute_draft_id: HexBinary,
+    pub tribute_draft_id: Base58Binary,
     /// Owner is a derivative address on L2 network based on blake3 hashing
-    pub owner: String,
+    pub owner: Base58Binary,
     /// ISO 8601
     pub worldwide_day: Iso8601Date,
     /// ISO 4217
@@ -20,5 +21,5 @@ pub struct TributeInputPayload {
     pub nominal_base_qty: Uint64,
     /// Amount expressed in fractional units, `0 >= nominal_atto_qty < 1e18`
     pub nominal_atto_qty: Uint128,
-    pub cu_hashes: Vec<HexBinary>,
+    pub cu_hashes: Vec<Base58Binary>,
 }
