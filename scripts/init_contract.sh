9fcfd5bb227d21e72b5156f372d2aa769365e173
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-08-25 14:04:02 +0100

    chore: add configurable binary support in deploy scripts and workflows

--- scripts/init_contract.sh
+++ scripts/init_contract.sh
@@ -5,10 +5,12 @@ label=$1
 code_id=$2
 payload=$3
 
-TX_HASH=$(outbe-noded tx wasm instantiate \
+binary=${BINARY:-outbe-noded}
+
+TX_HASH=$($binary tx wasm instantiate \
   $code_id "$payload" \
   --label "$label" \
-  --from ci --keyring-backend test -y --admin $(outbe-noded keys show --keyring-backend test ci -a) \
+  --from ci --keyring-backend test -y --admin $($binary keys show --keyring-backend test ci -a) \
   --node $RPC --chain-id $CHAIN_ID --gas-prices 0.25$FEE_DENOM --gas auto --gas-adjustment 1.3 --output json \
   | jq -r '.txhash')
 
@@ -16,7 +18,7 @@ sleep 7
 
 # Query a created contract
 # NB: we also need to filter by code_id because it may create several contracts under the hood
-CONTRACT_ADDRESS=$(outbe-noded query tx --type=hash $TX_HASH --node $RPC --output json \
+CONTRACT_ADDRESS=$($binary query tx --type=hash $TX_HASH --node $RPC --output json \
   | jq -r ".events[] | select(.type == \"instantiate\" and .attributes[].key == \"code_id\" and .attributes[].value == \"$code_id\")  | .attributes[] | select(.key == \"_contract_address\") | .value")
 
 echo $CONTRACT_ADDRESS
--- scripts/store_code.sh
+++ scripts/store_code.sh
@@ -3,14 +3,16 @@ set -e
 
 filename=$1
 
-TX_HASH=$(outbe-noded tx wasm store $filename \
+binary=${BINARY:-outbe-noded}
+
+TX_HASH=$($binary tx wasm store $filename \
   -y --from ci --keyring-backend test --broadcast-mode sync \
   --node $RPC --chain-id $CHAIN_ID --gas-prices 0.25$FEE_DENOM --gas auto --gas-adjustment 1.3 --output json \
   | jq -r '.txhash')
 
 sleep 7
 
-CODE_ID=$(outbe-noded query tx --type=hash $TX_HASH --node $RPC --output json | \
+CODE_ID=$($binary query tx --type=hash $TX_HASH --node $RPC --output json | \
   jq -r '.events[] | select(.type == "store_code") | .attributes[] | select(.key == "code_id") | .value')
 
 echo $CODE_ID
