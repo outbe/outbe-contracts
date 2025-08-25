2297bcd70d9e164c4dca5a9d8bcd0b15c69349cd
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-08-25 15:04:02 +0100

    fix script

--- scripts/store_code.sh
+++ scripts/store_code.sh
@@ -5,6 +5,12 @@ filename=$1
 
 binary=${BINARY:-outbe-noded}
 
+json_filter='.events[] | select(.type == "store_code") | .attributes[] | select(.key == "code_id") | .value'
+# code is is located in the logs for SEI
+if [ "$binary" != "outbe-noded" ]; then
+  json_filter=".logs[] | $json_filter"
+fi
+
 TX_HASH=$($binary tx wasm store $filename \
   -y --from ci --keyring-backend test --broadcast-mode sync \
   --node $RPC --chain-id $CHAIN_ID --gas-prices 0.25$FEE_DENOM --gas auto --gas-adjustment 1.3 --output json \
@@ -12,7 +18,6 @@ TX_HASH=$($binary tx wasm store $filename \
 
 sleep 7
 
-CODE_ID=$($binary query tx --type=hash $TX_HASH --node $RPC --output json | \
-  jq -r '.events[] | select(.type == "store_code") | .attributes[] | select(.key == "code_id") | .value')
+CODE_ID=$($binary query tx --type=hash $TX_HASH --node $RPC --output json | jq -r "$json_filter")
 
 echo $CODE_ID
