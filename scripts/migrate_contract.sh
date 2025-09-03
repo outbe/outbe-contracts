65694e60b708bbb70d6110b44c4596ddfd95fb77
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-09-03 11:32:25 +0100

    minor

--- scripts/migrate_contract.sh
+++ scripts/migrate_contract.sh
@@ -12,5 +12,3 @@ $binary tx wasm migrate $contract_address \
   $code_id "$payload" \
   --from ci --keyring-backend test -y \
   --node $RPC --chain-id $CHAIN_ID --gas-prices 0.25$FEE_DENOM --gas auto --gas-adjustment 1.3 --output json
-
-sleep 5
