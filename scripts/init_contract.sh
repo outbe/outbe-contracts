4e893b366c8e4ecd2e1a50b039b7b45f5e643177
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-09-08 14:06:46 +0100

    chore: improve shell scripts allowing fail in case of errors

--- scripts/init_contract.sh
+++ scripts/init_contract.sh
@@ -7,12 +7,12 @@ payload=$3
 
 binary=${BINARY:-outbe-noded}
 
-TX_HASH=$($binary tx wasm instantiate \
+RESPONSE=$($binary tx wasm instantiate \
   $code_id "$payload" \
   --label "$label" \
   --from ci --keyring-backend test -y --admin $($binary keys show --keyring-backend test ci -a) \
-  --node $RPC --chain-id $CHAIN_ID --gas-prices 0.25$FEE_DENOM --gas auto --gas-adjustment 1.3 --output json \
-  | jq -r '.txhash')
+  --node $RPC --chain-id $CHAIN_ID --gas-prices 0.25$FEE_DENOM --gas auto --gas-adjustment 1.3 --output json)
+TX_HASH=$(echo "$RESPONSE" | jq -r '.txhash')
 
 sleep 7
 
@@ -24,6 +24,7 @@ fi
 
 # Query a created contract
 # NB: we also need to filter by code_id because it may create several contracts under the hood
-CONTRACT_ADDRESS=$($binary query tx --type=hash $TX_HASH --node $RPC --output json | jq -r "$json_filter")
+RESPONSE=$($binary query tx --type=hash $TX_HASH --node $RPC --output json)
+CONTRACT_ADDRESS=$(echo "$RESPONSE"| jq -r "$json_filter")
 
 echo $CONTRACT_ADDRESS
