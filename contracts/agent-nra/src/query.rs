1e6a75c3b494f43e115e2b2dea7d937205a58384
benachyv <benachyv@proton.me>
2025-09-09 15:40:37 +0300

    fix query

--- contracts/agent-nra/src/query.rs
+++ contracts/agent-nra/src/query.rs
@@ -48,6 +48,7 @@ pub enum QueryMsg {
     EnsureActiveNra { address: Addr },
 }
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::ListAllApplications {
