75dd1415d44569812895490e5d38a3a6731d0aad
benachyv <benachyv@proton.me>
2025-09-17 07:53:58 +0300

    fix wasm check

--- contracts/agent-nra/examples/schema.rs
+++ contracts/agent-nra/examples/schema.rs
@@ -1,12 +1,8 @@
-use agent_nra::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg};
 use agent_nra::query::QueryMsg;
 use cosmwasm_schema::write_api;
 
 fn main() {
     write_api! {
-        instantiate: InstantiateMsg,
         query: QueryMsg,
-        execute: ExecuteMsg,
-        migrate: MigrateMsg,
     }
 }
