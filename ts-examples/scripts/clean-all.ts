cb83f415dbb65a1603826aaacdabe1f084767125
Latex Stone <171574715+latexstone@users.noreply.github.com>
2025-09-29 17:54:25 +0100

    feat: refactor `clean-all` script by modularizing burn logic with batch processing

--- ts-examples/scripts/clean-all.ts
+++ ts-examples/scripts/clean-all.ts
@@ -1,9 +1,6 @@
-import {
-  getContractAddresses,
-  initClient,
-} from "../lib/clientService";
+import {getContractAddresses, initClient,} from "../lib/clientService";
 import {TributeClient} from "../clients/tribute/Tribute.client";
-import {ExecuteResult} from "@cosmjs/cosmwasm-stargate";
+import {ExecuteResult, SigningCosmWasmClient} from "@cosmjs/cosmwasm-stargate";
 import {NodClient} from "../clients/nod/Nod.client";
 import {MetadosisClient} from "../clients/metadosis/Metadosis.client";
 import {TX_FEE} from "../config";
@@ -13,25 +10,41 @@ import {TributeFactoryClient} from "../clients/tribute-factory/TributeFactory.cl
 async function main() {
   const {walletClient, account} = await initClient()
 
-  let tributeContractAddress = await getContractAddresses('TRIBUTE_CONTRACT_ADDRESS')
-  let tributeClient = new TributeClient(walletClient, account.address, tributeContractAddress)
-  let tx: ExecuteResult = await tributeClient.burnAll(TX_FEE)
-  console.log("Burn all tribute, tx", tx.transactionHash)
-
-  let tributeFactoryContractAddress = await getContractAddresses('TRIBUTE_FACTORY_CONTRACT_ADDRESS')
-  let tributeFactoryClient = new TributeFactoryClient(walletClient, account.address, tributeFactoryContractAddress)
-  let tx1: ExecuteResult = await tributeFactoryClient.burnAll(TX_FEE)
-  console.log("Burn tribute factory, tx", tx1.transactionHash)
-
-  let nodContractAddress = await getContractAddresses('NOD_CONTRACT_ADDRESS')
-  let nodClient = new NodClient(walletClient, account.address, nodContractAddress)
-  let tx2: ExecuteResult = await nodClient.burnAll(TX_FEE)
-  console.log("Burn all nod, tx", tx2.transactionHash)
+  await burnTributes(walletClient, account.address)
+  await burnTributeFactory(walletClient, account.address)
+  await burnNods(walletClient, account.address)
 
   let metadosisContractAddress = await getContractAddresses('METADOSIS_CONTRACT_ADDRESS')
   let metadosisClient = new MetadosisClient(walletClient, account.address, metadosisContractAddress)
   let tx3: ExecuteResult = await metadosisClient.burnAll(TX_FEE)
   console.log("Burn metadosis, tx", tx3.transactionHash)
+}
+
+async function burnTributes(walletClient: SigningCosmWasmClient, address: string) {
+  let tributeContractAddress = await getContractAddresses('TRIBUTE_CONTRACT_ADDRESS')
+  let tributeClient = new TributeClient(walletClient, address, tributeContractAddress)
+  do {
+    let tx: ExecuteResult = await tributeClient.burnAll({batchSize: 50}, TX_FEE)
+    console.log("Burned 50 tributes ..., tx", tx.transactionHash)
+  } while (await tributeClient.numTokens().then(r => r.count > 0))
+  console.log("Burning tributes done ✅")
+}
+
+async function burnTributeFactory(walletClient: SigningCosmWasmClient, address: string) {
+  let tributeFactoryContractAddress = await getContractAddresses('TRIBUTE_FACTORY_CONTRACT_ADDRESS')
+  let tributeFactoryClient = new TributeFactoryClient(walletClient, address, tributeFactoryContractAddress)
+  let tx1: ExecuteResult = await tributeFactoryClient.burnAll(TX_FEE)
+  console.log("Burn tribute factory, tx", tx1.transactionHash)
+}
+
+async function burnNods(walletClient: SigningCosmWasmClient, address: string) {
+  let contractAddress = await getContractAddresses('NOD_CONTRACT_ADDRESS')
+  let client = new NodClient(walletClient, address, contractAddress)
+  do {
+    let tx: ExecuteResult = await client.burnAll({batchSize: 50}, TX_FEE)
+    console.log("Burned 50 Nods ..., tx", tx.transactionHash)
+  } while (await client.numTokens().then(r => r.count > 0))
+  console.log("Burning Nods done ✅")
 }
 
 main();
