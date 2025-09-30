c66c3d690f5a5e42e76877d67cf3b378e52be538
latexstone <171574715+latexstone@users.noreply.github.com>
2025-09-30 16:25:36 +0100

    feat: add functionality to query and update Coen/USDC and XAU/USD rates in oracle script (#131)

--- ts-examples/scripts/oracle-update-price.ts
+++ ts-examples/scripts/oracle-update-price.ts
@@ -1,15 +1,19 @@
 import {getContractAddresses, initClient} from "../lib/clientService";
-import {CosmWasmClient} from "@cosmjs/cosmwasm-stargate";
 import {PriceOracleClient, PriceOracleQueryClient} from "../clients/price-oracle/PriceOracle.client";
 import {TX_FEE} from "../config";
 
 async function main() {
   const {walletClient, account} = await initClient()
+  let contractAddress = await getContractAddresses('PRICE_ORACLE_CONTRACT_ADDRESS')
+  let client = new PriceOracleClient(walletClient, account.address, contractAddress)
+  let queryClient = new PriceOracleQueryClient(walletClient, contractAddress)
 
-  console.log("Current Rate:", await queryActualRate(walletClient))
+  console.log("Current Coen/USDC Rate:", await queryCoenRate(queryClient))
+  console.log("Current XAU/USD Rate:", await queryXauRate(queryClient))
 
-  let client = new PriceOracleClient(walletClient, account.address, await getContractAddresses('PRICE_ORACLE_CONTRACT_ADDRESS'))
+  console.log("Updating rates...")
 
+  // Update Coen/USDC rate
   await client.updatePrice({
     price: "0.013",
     token1: {
@@ -20,18 +24,41 @@ async function main() {
     },
   }, TX_FEE)
 
-  console.log("New Rate:", await queryActualRate(walletClient))
+  // Update XAU/USD rate
+  await client.updatePrice({
+    price: "3305.90",
+    token1: {
+      commodity: "xau"
+    },
+    token2: {
+      fiat: "usd"
+    },
+  }, TX_FEE)
+
+  console.log("New Coen/USDC Rate:", await queryCoenRate(queryClient))
+  console.log("New XAU/USD Rate:", await queryCoenRate(queryClient))
 }
 
-export async function queryActualRate(walletClient: CosmWasmClient): Promise<number> {
-  let address = await getContractAddresses('PRICE_ORACLE_CONTRACT_ADDRESS')
-  let client = new PriceOracleQueryClient(walletClient, address)
-  let response = await client.getLatestPrice({
+export async function queryCoenRate(queryClient: PriceOracleQueryClient): Promise<number> {
+  let response = await queryClient.getLatestPrice({
     token1: {
       native: "coen"
     },
     token2: {
       native: "usdc"
+    },
+  })
+
+  return parseFloat(response.price)
+}
+
+export async function queryXauRate(queryClient: PriceOracleQueryClient): Promise<number> {
+  let response = await queryClient.getLatestPrice({
+    token1: {
+      commodity: "xau"
+    },
+    token2: {
+      fiat: "usd"
     },
   })
 
